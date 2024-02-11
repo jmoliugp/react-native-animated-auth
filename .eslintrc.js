28a888ce321150741d04d39809f68322da0968df
Solo <juan@xmartlabs.com>
2024-02-10 21:33:23 -0300

    customize linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'simple-import-sort',
+    'sort-keys-fix',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    curly: 'off',
+    'no-duplicate-imports': ['error', {includeExports: true}],
+    'no-nested-ternary': 'error',
+    'no-shadow': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    semi: 'off',
+    'simple-import-sort/imports': 'error',
+    'sort-keys': 'error',
+  },
+}
